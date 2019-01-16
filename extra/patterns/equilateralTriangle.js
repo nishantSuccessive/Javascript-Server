a067469aa44a372fba7bea5be9c049d8f7d97e98
nishantSuccessive <nishant.saxena@successive.tech>
2019-01-16 14:23:12 +0530

    Created a two files in which patterns of diamond and equilateral triangle are made

---
+++ extra/patterns/equilateralTriangle.js
@@ -0,0 +1,19 @@
+function EquilateralTriangle(row)
+{
+    let pattern =  " ";
+    let temp = row;
+    for(let index = 0; i<row ; index++) {
+        for(let temp1 = temp; temp1>0; temp1--) {
+             pattern = pattern + " ";
+         }
+
+        for(let temp2 = index; temp2>=0; temp2--) {
+
+            pattern = pattern + "* ";
+        }
+        console.log(pattern);
+        temp--;
+        pattern = " "
+    }
+}
+EquilateralTriangle(10)
